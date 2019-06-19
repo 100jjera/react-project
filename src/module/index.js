6e611a9526c627f775417b80a20b362568e079f9
BAEK <100jaehun@naver.com>
2019-06-19 21:30:55 +0900

    redux store 설정

---
+++ src/module/index.js
@@ -0,0 +1,26 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux"; 
+import penderMiddleware, { penderReducer } from 'redux-pender'; 
+ 
+ 
+ 
+export function configureStore() { 
+    const middleware = applyMiddleware(penderMiddleware()) 
+ 
+    const composed = window.__REDUX_DEVTOOLS_EXTENSION__ ? 
+        compose( 
+            middleware, 
+            window.__REDUX_DEVTOOLS_EXTENSION__() 
+        ) : 
+        middleware; 
+ 
+ 
+ 
+    //createStore에 첫번 째 인자는 리듀서,두번재 인자는 미들웨어 
+    return createStore( 
+        combineReducers({ 
+            pender: penderReducer 
+ 
+        }), 
+        composed 
+    ) 
+} 
